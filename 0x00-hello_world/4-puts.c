6e2b56e939b1072de6309f26eaf1d1fbf4c7231b
bereketgg <berketket@gmail.com>
2022-03-11 02:37:00 +0300

    still trying to please betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /*
  * main - starting point
- *
  * Return: 0 if the code runs without error
  */
+ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
