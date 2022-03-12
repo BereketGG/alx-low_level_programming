708ef4192db9bfbeea3b721e4b3c09e7c3840d22
bereketgg <berketket@gmail.com>
2022-03-12 05:22:14 +0300

    writing em digits

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,21 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-
 	int num = 0;
 
-	for (num = '0'; num =< '9'; num++)
+	for (num <= 9)
 	{
 		putchar(num);
+		num++;
 	}
 	putchar('\n');
-return (0);
+
+	return (0);
 }
+
