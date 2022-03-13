7838083e71f82139afeb82957b2cf72cae511351
bereketgg <berketket@gmail.com>
2022-03-14 01:53:40 +0300

    with betty's blessing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - starting point
+ * Return: 0 if the code runs without error
+ */
+
+int main(void)
+{
+	char text[] = "and that piece of art is useful\"- Dora Korpar, 2015-10-19";
+
+	printf("%s", text);
+
+return (1);
+}
