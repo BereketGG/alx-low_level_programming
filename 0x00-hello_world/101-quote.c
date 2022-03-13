ab560931dc2b3aa10285b7b6afff773e5346d347
bereketgg <berketket@gmail.com>
2022-03-14 02:00:34 +0300

    with betty's blessing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19 ";
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19
+	";
 
 	printf("%s", text);
 
