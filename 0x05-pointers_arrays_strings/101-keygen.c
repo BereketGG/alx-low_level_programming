fe0528482c16b20e58efc6a48b69667f9cc36033
Bereket <berketket@gmail.com>
2022-03-22 21:46:18 +0300

    p

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random passwords
+ * for program crackme.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	int N = rand() % 25;
+	char *Letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *Number = "123456789";
+	char *symbol = "!@#$^&*?";
+	char *letter = "abcdefghijklmnopqrstuvwxyz";
+	int randomizer = rand() % 4;
+	char password[50];
+
+	for (i = 0; i < N; i++)
+	{
+		if (randomizer == 1) 
+		{
+			password[i] = Number[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2) 
+		{
+			password[i] = symbol[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);	
+		}
+		else if (randomizer == 3) 
+		{
+			password[i] = Letter[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else 
+		{
+			password[i] = letter[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+	return 0;
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that prints a string
+ * in reverse.
+ *
+ * @s: input to above function.
+ *
+ * Return: void.
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int tmp;
+	int len = 0;
+	int x;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		len++;
+	}
+	len = len - 1;
+	for (i = 0; i <= len / 2; i++)
+	{
+		x = len - i;
+		tmp = s[i];
+		s[i] = s[x];
+		s[x] = tmp;
+	}
+}
